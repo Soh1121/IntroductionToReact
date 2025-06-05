21d249ffde96b47a6c57fd5e6f56bb68045f409e
Soh1121 <satou.shg@gmail.com>
2025-06-06 07:06:46 +0900

    ✨feat: const 変数に配列を代入するときの注意点を学習

--- src/index.js
+++ src/index.js
@@ -31,11 +31,17 @@
 // // constは再宣言不可
 // const val3 = "const変数を再宣言";
 
-// const で定義したオブジェクトはプロパティの変更が可能
-const val4 = {
-	name: "じゃけぇ",
-	age: 31,
-};
-val4.name = "jak";
-val4.address = "Hiroshima";
-console.log(val4);
+// // const で定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+// 	name: "じゃけぇ",
+// 	age: 31,
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
