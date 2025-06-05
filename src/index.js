91064b23eb39f48e987ecf9379477b506efa5212
Soh1121 <satou.shg@gmail.com>
2025-06-06 07:00:38 +0900

    ✨feat: const 変数を学習

--- src/index.js
+++ src/index.js
@@ -12,12 +12,21 @@
 // var val1 = "var変数を再宣言";
 // console.log(val1);
 
-let val2 = "let変数";
-console.log(val2);
+// let val2 = "let変数";
+// console.log(val2);
 
-// letは上書き可能
-val2 = "let変数を上書き";
-console.log(val2);
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
 
-// letは再宣言不可能
+// // letは再宣言不可能
 // let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// const変数は上書き不可
+val3 = "const変数を上書き";
+
+// constは再宣言不可
+const val3 = "const変数を再宣言";
