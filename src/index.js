f17a1f916d727ecc9aa87a1f5a4537f2399ab05b
Soh1121 <satou.shg@gmail.com>
2025-06-09 06:53:46 +0900

    ✨feat: 引数複数版

--- src/index.js
+++ src/index.js
@@ -76,5 +76,8 @@
 // console.log(func1("func1です"));
 
 // アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です"));
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
