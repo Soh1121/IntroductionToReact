ddc137ee663d36eecdce395f5f43a0b32464664d
Soh1121 <satou.shg@gmail.com>
2025-06-09 06:55:40 +0900

    ✨feat: 単一式が複数行に渡る場合は、カッコで括るとよい

--- src/index.js
+++ src/index.js
@@ -79,5 +79,8 @@
 // const func2 = (str) => str;
 // console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
+const func3 = (num1, num2) => ({
+	hoge: num1,
+	fuga: num2,
+});
 console.log(func3(10, 20));
