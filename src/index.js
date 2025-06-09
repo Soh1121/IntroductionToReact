19116c7166916e8ec4e7dd2eac60bb5477c5c340
Soh1121 <satou.shg@gmail.com>
2025-06-10 06:46:52 +0900

    ✨feat: オブジェクトの分割代入を学習

--- src/index.js
+++ src/index.js
@@ -95,3 +95,7 @@ const myProfile = {
 
 const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 console.log(message1);
+
+const { age, name } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
