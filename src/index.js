e533fdbcbfa9826fb1fb10335b2aaa5b3ba2a0c0
Soh1121 <satou.shg@gmail.com>
2025-06-10 06:51:40 +0900

    ✨feat: 配列の分割代入を学習

--- src/index.js
+++ src/index.js
@@ -88,14 +88,23 @@
 /**
  * 分割代入
  */
-const myProfile = {
-	name: "じゃけぇ",
-	age: 31,
-};
+// const myProfile = {
+// 	name: "じゃけぇ",
+// 	age: 31,
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-console.log(message1);
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
 
-const { age, name } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const { age, name } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myProfile = ["じゃけぇ", 31];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
