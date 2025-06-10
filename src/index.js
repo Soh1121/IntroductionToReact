2db5669cd0a85c3e180b025edff8f4c32a9a4f7a
Soh1121 <satou.shg@gmail.com>
2025-06-11 06:57:53 +0900

    ✨feat: スプレッド構文を使えば参照をコピーしていないことを確認

--- src/index.js
+++ src/index.js
@@ -159,12 +159,14 @@ const arr4 = [10, 20];
 const arr5 = [30, 40];
 
 const arr6 = [...arr4];
-// console.log(arr6);
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
 
-const arr7 = [...arr4, ...arr5];
+// const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
 
-const arr8 = arr4;
-arr8[0] = 100;
-console.log(arr8);
-console.log(arr4);
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
