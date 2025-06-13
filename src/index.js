a074e7d3ab190562d39c5c7872d70ef3d80d2b19
Soh1121 <satou.shg@gmail.com>
2025-06-14 06:56:44 +0900

    ✨feat: map を学習

--- src/index.js
+++ src/index.js
@@ -175,6 +175,12 @@
  * mapやfilterを使った配列の処理
  */
 const nameArr = ["田中", "山田", "じゃけぇ"];
-for (let index = 0; index < nameArr.length; index++) {
-	console.log(nameArr[index]);
-}
+// for (let index = 0; index < nameArr.length; index++) {
+// 	console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+// 	return name;
+// });
+nameArr.map((name) => console.log(name));
+// console.log(nameArr2);
