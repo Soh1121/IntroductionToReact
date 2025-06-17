5442a8492cc2bfd4f18dfd05ee6d53b46e5b1c46
Soh1121 <satou.shg@gmail.com>
2025-06-18 07:04:05 +0900

    ✨feat: 三項演算子の結果を返す関数

--- src/index.js
+++ src/index.js
@@ -206,9 +206,14 @@
 // const val1 = 1 > 2 ? "trueです" : "falseです";
 // console.log(val1);
 
-const num = "1300";
+// const num = 1300;
 // console.log(num.toLocaleString());
 
-const formattedNum =
-	typeof num === "number" ? num.toLocaleString : "数値を入力してください";
-console.log(formattedNum);
+// const formattedNum =
+// 	typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+	return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲ないです";
+};
+console.log(checkSum(60, 50));
