c0a8863a37aefe87546010733e7127c8f0d03937
Soh1121 <satou.shg@gmail.com>
2025-07-17 07:10:38 +0900

    ✨feat: 基本的な記述を追加

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	const root = createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<h1>こんにちは!</h1>
+		</StrictMode>,
+	);
+}
