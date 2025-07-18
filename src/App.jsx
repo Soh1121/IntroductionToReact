9d8b41c82066c14125282f658ebfc05496daa157
Soh1121 <satou.shg@gmail.com>
2025-07-19 06:37:46 +0900

    ✨feat: state の初期化を学習

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+	const [num, setNum] = useState(0);
 	const onClickButton = () => alert();
 	return (
 		<>
@@ -10,6 +12,7 @@ export const App = () => {
 			<button type="button" onClick={onClickButton}>
 				ボタン
 			</button>
+			<p>{num}</p>
 		</>
 	);
 };
