74cda380fecddd01c4d4ffdfda09984341ae7d22
Soh1121 <satou.shg@gmail.com>
2025-07-25 06:39:54 +0900

    🔨refactor: 未完了タスク部分をコンポーネント化

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./styles.css";
+import { IncompleteTodos } from "./components/IncompleteTodos";
 import { InputTodo } from "./components/InputTodo";
 
 export const Todo = () => {
@@ -53,24 +54,13 @@ export const Todo = () => {
 				onChange={onChangeText}
 				onClick={onClickAdd}
 			/>
-			<div className="incomplete-area">
-				<p className="title">未完了のTODO</p>
-				<ul>
-					{incompleteTodos.map((todo, index) => (
-						<li key={todo}>
-							<div className="list-row">
-								<p className="todo-item">{todo}</p>
-								<button type="button" onClick={() => onClickComplete(index)}>
-									完了
-								</button>
-								<button type="button" onClick={() => onClickDelete(index)}>
-									削除
-								</button>
-							</div>
-						</li>
-					))}
-				</ul>
-			</div>
+
+			<IncompleteTodos
+				todos={incompleteTodos}
+				onClickComplete={onClickComplete}
+				onClickDelete={onClickDelete}
+			/>
+
 			<div className="complete-area">
 				<p className="title">完了のTODO</p>
 				<ul>
